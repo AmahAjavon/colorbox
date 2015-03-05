72170ac697abed491366cf7701bee2223c1924bf
AmahAjavon <aj_ajavon@hotmail.com>
2015-03-05 14:35:24 -0800

    functional color strings display colors

--- client/main.js
+++ client/main.js
@@ -1,1 +1,20 @@
-console.log('hello, world');
+'use strict';
+
+$(document).ready(init);
+
+var colors = [];
+
+function init() {
+
+   $("#addBox").click(function() {
+     var color = $('#colors').val();
+     var sp = color.split(',');
+    sp.forEach(function(color) {
+      var $boxColor= $('<div>');
+      $boxColor.css({'background-color': color,'margin': '5px','float':'left', 'height':'80px','width': '80px', 'font-size': '15px'});
+       $('#boxes').append($boxColor);
+
+    });
+
+});
+}
